8b1faf843929e95b54d6285fca55ac52af86e543
Astro <astroval0@proton.me>
2025-10-28 08:27:14 +0000

    chore: update comment for more clarity

--- src/common.h
+++ src/common.h
@@ -14,7 +14,7 @@ using op = std::optional<wstr>;
 // spectre's steam appid
 inline constexpr int APP_ID = 2641470;
 inline constexpr auto APP_ID_STR = L"2641470";
-// this is the patched BE dll that bypasses the game's init checks.
+// this is the patched BE dll that bypasses the game's init checks and applies the hook.
 inline constexpr auto RELEASE_URL = L"https://github.com/astroval0/SpectrePatcher/releases/latest/download/BEClient_x64.dll";
 // pragmabackend addr
 inline constexpr auto BACKEND_ADDRESS = L"http://game.spectre.astro-dev.uk:8081";
